7e865d12b184912003ccdd80066e45d7c730aff8
hoani <hoani.bryson@gmail.com>
2022-06-22 17:34:30 +1200

    handle missing time field

--- line.go
+++ line.go
@@ -3,15 +3,19 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"strconv"
+	"strings"
 	"time"
 )
 
 type f struct {
-	locale *time.Location
+	index      int64
+	locale     *time.Location
+	timeLength int
 }
 
 type logData struct {
-	f       *f
+	locale  *time.Location
 	Time    *time.Time
 	Level   string
 	Message string
@@ -31,7 +35,7 @@ func (d *logData) UnmarshalJSON(data []byte) error {
 			d.Time = &tvalue
 			delete(m, "time")
 		case string:
-			tvalue, err := time.ParseInLocation(time.RFC3339Nano, value, d.f.locale)
+			tvalue, err := time.ParseInLocation(time.RFC3339Nano, value, d.locale)
 			if err != nil {
 				return err
 			}
@@ -42,7 +46,14 @@ func (d *logData) UnmarshalJSON(data []byte) error {
 
 	if mvalue, ok := m["level"]; ok {
 		if value, ok := mvalue.(string); ok {
-			d.Level = value
+			d.Level = strings.ToUpper(value)
+			if len(d.Level) > 5 {
+				if strings.HasPrefix(d.Level, "WARN") {
+					d.Level = "WARN"
+				} else {
+					d.Level = d.Level[:5]
+				}
+			}
 			delete(m, "level")
 		}
 	}
@@ -60,23 +71,32 @@ func (d *logData) UnmarshalJSON(data []byte) error {
 
 func NewLineFormatter(locale *time.Location) *f {
 	return &f{
+		index:  1,
 		locale: locale,
 	}
 }
 
 func (f *f) Format(input []byte) (string, error) {
+	defer func() { f.index++ }()
 
 	data := logData{
-		f: f,
+		locale: f.locale,
 	}
 	if err := json.Unmarshal(input, &data); err != nil {
 		fmt.Printf("failed to unmarshal, err: %v\n", err)
 		return "", err
 	}
 
-	res := fmt.Sprintf("[%v] %v: %v", data.Time.Format(time.StampMilli), data.Level, data.Message)
-	if len(data.Meta) != 0 {
-		res += fmt.Sprintf(", %v", data.Meta)
+	var output string
+	if data.Time == nil {
+		indexStr := strconv.FormatInt(f.index, 10)
+		output += "[" + strings.Repeat(" ", len(time.StampMilli)-len(indexStr)) + indexStr + "]"
+	} else {
+		output += "[" + data.Time.Format(time.StampMilli) + "]"
 	}
-	return res, nil
+	output += fmt.Sprintf(" %-5v %v", data.Level, data.Message)
+	if len(data.Meta) != 0 {
+		output += fmt.Sprintf(", %v", data.Meta)
+	}
+	return output, nil
 }
--- line_test.go
+++ line_test.go
@@ -8,17 +8,49 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func Test_ConvertLine(t *testing.T) {
-	data := map[string]interface{}{
-		"time":    time.Unix(0, 0).Add(time.Second + time.Millisecond + time.Microsecond),
-		"level":   "error",
-		"message": "something went wrong",
+func Test_ConvertLine_Success(t *testing.T) {
+	testCases := []struct {
+		name     string
+		data     map[string]interface{}
+		expected string
+	}{
+		{
+			name: "simple with error",
+			data: map[string]interface{}{
+				"time":    time.Unix(0, 0).Add(time.Second + time.Millisecond + time.Microsecond),
+				"level":   "error",
+				"message": "something went wrong",
+			},
+			expected: "[Jan  1 12:00:01.001] ERROR something went wrong",
+		},
+		{
+			name: "info in 2016",
+			data: map[string]interface{}{
+				"time":    time.Date(2016, 1, 10, 7, 7, 7, 777e6, time.UTC),
+				"level":   "info",
+				"message": "everybody knows me now",
+			},
+			expected: "[Jan 10 07:07:07.777] INFO  everybody knows me now",
+		},
+		{
+			name: "no time field",
+			data: map[string]interface{}{
+				"level":   "warning",
+				"message": "you're almost there, but don't panic",
+			},
+			expected: "[                  1] WARN  you're almost there, but don't panic",
+		},
 	}
-	b, err := json.Marshal(data)
-	assert.Nil(t, err)
 
-	c := NewLineFormatter(time.UTC)
-	s, err := c.Format(b)
-	assert.Nil(t, err)
-	assert.Equal(t, "[Jan  1 12:00:01.001] error: something went wrong", s)
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			b, err := json.Marshal(tc.data)
+			assert.Nil(t, err)
+
+			c := NewLineFormatter(time.UTC)
+			s, err := c.Format(b)
+			assert.Nil(t, err)
+			assert.Equal(t, tc.expected, s)
+		})
+	}
 }
